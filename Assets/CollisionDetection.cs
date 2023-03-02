c945fc20b01730f33801ee8b7a7b417d8b49fb8d
jsanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2023-03-02 20:49:12 -0300

    Initial commit

---
+++ Assets/CollisionDetection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "gem-1" || col.gameObject.name == "Zorrito")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+    void OnCollisionExit()
+    {
+
+    }
+
+    void OnCollisionStay()
+    {
+
+    }
+}
---
+++ Assets/DeteccionDeGemas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DeteccionDeGemas : MonoBehaviour
+{
+    public int score;
+    public TextMeshProUGUI txtScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtScore.text = 0.ToString(); ;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Gema")
+        {
+            score++;
+            txtScore.text = score.ToString();
+        }
+    }
+}
